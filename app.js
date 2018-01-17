0b2c52f47afc0f04eb04f9b9d0a69d9e291a2ae8
Harish Veeramani <hdv987@gmail.com>
2018-01-17 14:35:12 -0600

    Add templates and template engine

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 //Vars
 const express = require('express');
+const exphbs = require('express-handlebars');
 const app = express();
 const port = 5000;
 
+//Handlebars Middleware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
 //Routes
 app.get('/', (req, res) => {
-    res.send("Test");
+    res.render("index", {
+        title: "Welcome to Quick Jot"
+    });
 });
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
