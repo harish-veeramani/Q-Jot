b85d6b4bc1d2b8469e84f2e4756394bd6bc71e55
Harish Veeramani <hdv987@gmail.com>
2018-01-17 18:24:56 -0600

    Connect to database

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 //Vars
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require("mongoose");
 const app = express();
 const port = 5000;
+
+mongoose.Promise = global.Promise;
+
+//Connect to MongoDB
+mongoose.connect("mongodb://localhost/quickjot-dev")
+    .then(() => console.log("Connected to database"))
+    .catch((error) => console.log(error));
 
 //Handlebars Middleware
 app.engine('handlebars', exphbs({
