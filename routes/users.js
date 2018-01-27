f9a90f18730d1d1ac947671f5d0241743cd1e7da
Harish Veeramani <hdv987@gmail.com>
2018-01-26 21:07:54 -0600

    Register form validation

--- routes/users.js
+++ routes/users.js
@@ -12,3 +12,26 @@ router.get('/login', (req, res) => {
 router.get('/register', (req, res) => {
     res.render("users/register");
 });
+
+router.post('/register', (req, res) => {
+    var errors = [];
+
+    if(req.body.password != req.body.password2){
+        errors.push({text: "Passwords do not match"})
+    }
+
+    if(req.body.password.length < 4){
+        errors.push({text: "Password must be at least 4 characters long"})
+    }
+
+    if(errors.length > 0){
+        res.render("users/register", {
+            errorList: errors,
+            name: req.body.name,
+            email: req.body.email
+        });
+    } else {
+        res.send("passed");
+    }
+
+});
